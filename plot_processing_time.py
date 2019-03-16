d7ea624477a24460bec551e8c6ef0edc1b760a08
stfate <stfate@gmail.com>
2019-03-16 10:55:51 +0900

    initial commit

---
+++ plot_processing_time.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+@package
+@brief
+@author stfate
+"""
+
+import scipy as sp
+import json
+import umi.plot as pp
+
+
+if __name__ == "__main__":
+    bhtsne_result = json.load(open("./result/bhtsne_results.json"))
+    bhtsne_samples = sp.array(list(bhtsne_result.keys())).astype(int)
+    bhtsne_proctimes = sp.array(list(bhtsne_result.values()))
+
+    tsnecuda_result = json.load(open("./result/tsnecuda_results.json"))
+    tsnecuda_samples = sp.array(list(tsnecuda_result.keys())).astype(int)
+    tsnecuda_proctimes = sp.array(list(tsnecuda_result.values()))
+
+    umap_result = json.load(open("./result/umap_results.json"))
+    umap_samples = sp.array(list(umap_result.keys())).astype(int)
+    umap_proctimes = sp.array(list(umap_result.values()))
+
+    fig,ax = pp.subplots()
+    ax.semilogx(bhtsne_samples, bhtsne_proctimes, label="Barnes-Hut t-SNE")
+    ax.semilogx(tsnecuda_samples, tsnecuda_proctimes, label="t-SNE-CUDA")
+    ax.semilogx(umap_samples, umap_proctimes, label="UMAP")
+    ax.set_xlabel("number of samples")
+    ax.set_ylabel("processing time (sec)")
+    ax.set_title("Processing time of t-SNE/t-SNE-CUDA/UMAP")
+    ax.grid(True)
+    ax.legend()
+    # pp.show()
+    pp.savefig("tsne-cuda-proctime.png")
---
+++ test_mnist_umap.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@package test_mnist_umap.py
+@brief
+@author stfate
+"""
+
+import scipy as sp
+from sklearn.datasets import fetch_mldata
+import json
+from tqdm import tqdm
+from umap import UMAP
+import umi.plot as pp
+
+import time
+
+
+if __name__ == "__main__":
+    COLORS = ["#000000", "#ffff33", "#66ff66", "#00ccff", "#6633ff", "#cc3399", "#ff3300", "#0066cc", "#9933cc", "#006633"]
+    mnist = fetch_mldata("MNIST original", data_home="./")
+    n_obs_all = len(mnist["data"])
+    n_obs_list = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 10000, 20000, 30000, 40000, 50000, 60000, n_obs_all]
+
+    result = {}
+    for n_obs in tqdm(n_obs_list):
+        mnist_data = mnist["data"][:n_obs]
+        mnist_labels = mnist["target"][:n_obs].astype(int)
+
+        st = time.clock()
+
+        model = UMAP(n_components=2)
+        predicted = model.fit_transform(mnist_data)
+
+        ed = time.clock()
+        result[n_obs] = ed-st
+
+        xmin = predicted[:,0].min()
+        xmax = predicted[:,0].max()
+        ymin = predicted[:,1].min()
+        ymax = predicted[:,1].max()
+
+        output_fn = f"mnist_umap_obs{n_obs}.png"
+        fig,ax = pp.subplots(figsize=(16,12))
+        unique_labels = sp.unique(mnist_labels)
+        for _label in unique_labels:
+            inds = sp.where(mnist_labels == _label)[0]
+            ax.scatter(predicted[inds,0], predicted[inds,1], c=COLORS[_label], label=_label)
+
+        ax.set_xlim(xmin, xmax)
+        ax.set_ylim(ymin, ymax)        
+        ax.set_xlabel("component 0")
+        ax.set_ylabel("component 1")
+        ax.set_title("MNIST UMAP visualization")
+        ax.legend()
+        pp.savefig(output_fn)
+
+    json.dump(result, open("umap_results.json", "w"), ensure_ascii=False, indent=2, sort_keys=True)
